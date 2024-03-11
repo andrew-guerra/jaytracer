805534d3274000d17faca94495f3b4b4591f0654
andrew-guerra <andrewjguerra10@gmail.com>
2024-03-11 11:51:06 -0500

    Add package information javadocs

---
+++ src/main/java/com/andrewguerra/jaytracer/math/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * A collection of vector, ray, and random utilities.
+ */
+package com.andrewguerra.jaytracer.math;
---
+++ src/main/java/com/andrewguerra/jaytracer/render/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Definition of render scene and scene enties, as well as utilies to render scenes.
+ */
+package com.andrewguerra.jaytracer.render;
